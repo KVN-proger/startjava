c1bf99be731b6b81bc6c2f8cee38665938f10065
Vitaly <supercyfqgth2010@yandex.ru>
2020-11-23 18:18:35 +0300

    correction of homework, added spaces

--- lesson 1/ConditionalStatement.java
+++ lesson 1/ConditionalStatement.java
@@ -1,34 +1,36 @@
 public class ConditionalStatement {
 	public static void main(String[] args) {
 		int age = 25;
-		int genderMale = 1;
-		float height = 1.7f;
-		boolean isGenderMale = genderMale ==1;
-		char firstLetterName = 'M';
 
-		if(age>20){
+		if(age > 20) {
 			System.out.println("Mark is over 20 years old");
 		}
 
-		if(isGenderMale){
+		boolean isGenderMale = true;
+
+		if(isGenderMale) {
 			System.out.println("Mark is man");
 		}
 
-		if(!isGenderMale){
+		if(!isGenderMale) {
 			System.out.println("This person is woman");
 		};
 
-		if(height<1.80f){
+		float height = 1.7f;
+
+		if(height < 1.80f) {
 			System.out.println("Mark is short");
-		}else{
+		} else {
 			System.out.println("Mark is tall");
 		}
 
-		if(firstLetterName == 'M'){
+		char firstLetterName = 'M';
+
+		if(firstLetterName == 'M') {
 			System.out.println("in the name Mark the first letter - M ");
-		}else if(firstLetterName == 'I'){
+		} else if(firstLetterName == 'I') {
 			System.out.println("in the name the first letter - I");
-		}else{
+		} else {
 			System.out.println("in the name the first letter is not I");
 		}
 
