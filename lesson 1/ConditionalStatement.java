a907ca0fa744711c0118c81a0bb36a363d1f05e4
Vitaly <supercyfqgth2010@yandex.ru>
2020-11-25 17:42:49 +0300

    empty lines removed

--- lesson 1/ConditionalStatement.java
+++ lesson 1/ConditionalStatement.java
@@ -1,23 +1,19 @@
 public class ConditionalStatement {
 	public static void main(String[] args) {
 		int age = 25;
-
 		if(age > 20) {
 			System.out.println("Mark is over 20 years old");
 		}
 
 		boolean isGenderMale = true;
-
 		if(isGenderMale) {
 			System.out.println("Mark is man");
 		}
-
 		if(!isGenderMale) {
 			System.out.println("This person is woman");
-		};
+		}
 
 		float height = 1.7f;
-
 		if(height < 1.80f) {
 			System.out.println("Mark is short");
 		} else {
@@ -25,7 +21,6 @@ public class ConditionalStatement {
 		}
 
 		char firstLetterName = 'M';
-
 		if(firstLetterName == 'M') {
 			System.out.println("in the name Mark the first letter - M ");
 		} else if(firstLetterName == 'I') {
@@ -33,6 +28,5 @@ public class ConditionalStatement {
 		} else {
 			System.out.println("in the name the first letter is not I");
 		}
-
 	}
 }
