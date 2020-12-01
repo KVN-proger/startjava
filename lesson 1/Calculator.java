9b1aa5b65e95c58c1a3deb146893d7c0a1d8a714
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-01 20:15:40 +0300

    added if else condition check

--- lesson 1/Calculator.java
+++ lesson 1/Calculator.java
@@ -4,26 +4,38 @@ public class Calculator {
 		int y = 5;
 		if (x + y == 15) {
 			System.out.println("sum " + x + " + " + y + " = 15");
+		} else {
+			System.out.println("the sign of the mathematical operation not the sum is selected");
 		}
 
 		if (x - y == 5) {
 			System.out.println("subtraction " + x + " - " + y + " = 5");
+		} else {
+			System.out.println("the sign of the mathematical operation not the subtraction is selected");
 		}
 
 		if (x * y == 50) {
 			System.out.println("multiplication " + x + " * " + y + " = 50");
+		} else {
+			System.out.println("the sign of the mathematical operation not the multiplication is selected");
 		}
 
 		if (x / y == 2) {
 			System.out.println("div " + x + " / " + y + " = 2");
+		} else {
+			System.out.println("the sign of the mathematical operation not the div is selected");
 		}
 
 		if (x * x == 100) {
 			System.out.println("exponentiation 2 number " + x + " = 100");
+		} else {
+			System.out.println("the exponentiation operation is not selected");
 		}
 
 		if (x % y == 0) {
 			System.out.println("remainder of division " + x + " % " + y + " = 0");
+		} else {
+			System.out.println("the operation for finding the remainder of a division is not selected");
 		}
 	}
 }
