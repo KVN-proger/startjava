a00d1a27fea852a636af97a9328d7058082adb81
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-03 13:23:33 +0300

    fixed bugs

--- lesson 1/Calculator.java
+++ lesson 1/Calculator.java
@@ -1,41 +1,57 @@
 public class Calculator {
 	public static void main(String[] args) {
+
 		int x = 10;
 		int y = 5;
-		if (x + y == 15) {
-			System.out.println("sum " + x + " + " + y + " = 15");
-		} else {
-			System.out.println("the sign of the mathematical operation not the sum is selected");
-		}
+		int z = 0;
+		char symbol;
 
-		if (x - y == 5) {
-			System.out.println("subtraction " + x + " - " + y + " = 5");
-		} else {
-			System.out.println("the sign of the mathematical operation not the subtraction is selected");
-		}
+		char [] symbolArray = { '+', '-', '*', '/', '^' , '%'};
 
-		if (x * y == 50) {
-			System.out.println("multiplication " + x + " * " + y + " = 50");
-		} else {
-			System.out.println("the sign of the mathematical operation not the multiplication is selected");
-		}
+		for (int i = 0; i < 6 ; i++) {
+			symbol = symbolArray[i];
 
-		if (x / y == 2) {
-			System.out.println("div " + x + " / " + y + " = 2");
-		} else {
-			System.out.println("the sign of the mathematical operation not the div is selected");
-		}
+			if (symbol == '+') {
+				z = x + y;
+				System.out.println("sum " + x + " + " + y + " = " + z);
+			} else {
+				System.out.println("the sign of the mathematical operation not the sum is selected");
+			}
 
-		if (x * x == 100) {
-			System.out.println("exponentiation 2 number " + x + " = 100");
-		} else {
-			System.out.println("the exponentiation operation is not selected");
-		}
+			if (symbol == '-') {
+				z = x - y;
+				System.out.println("subtraction " + x + " - " + y + " = " + z);
+			} else {
+				System.out.println("the sign of the mathematical operation not the subtraction is selected");
+			}
 
-		if (x % y == 0) {
-			System.out.println("remainder of division " + x + " % " + y + " = 0");
-		} else {
-			System.out.println("the operation for finding the remainder of a division is not selected");
+			if (symbol == '*') {
+				z = x * y;
+				System.out.println("multiplication " + x + " * " + y + " = " + z);
+			} else {
+				System.out.println("the sign of the mathematical operation not the multiplication is selected");
+			}
+
+			if (symbol == '/') {
+				z = x / y;
+				System.out.println("div " + x + " / " + y + " = " + z);
+			} else {
+				System.out.println("the sign of the mathematical operation not the div is selected");
+			}
+
+			if (symbol == '^') {
+				z = x * x;
+				System.out.println("exponentiation 2 number " + x + " = " + z);
+			} else {
+				System.out.println("the exponentiation operation is not selected");
+			}
+
+			if (symbol == '%') {
+				z = x % y;
+				System.out.println("remainder of division " + x + " % " + y + " = " + z);
+			} else {
+				System.out.println("the operation for finding the remainder of a division is not selected");
+			}
 		}
 	}
 }
