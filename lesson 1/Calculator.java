edf3f2a105ce04a2dbce9bfd6a1317ca96cd3864
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-03 20:14:31 +0300

    removed the spaces, renamed the array and variable

--- lesson 1/Calculator.java
+++ lesson 1/Calculator.java
@@ -1,54 +1,55 @@
 public class Calculator {
 	public static void main(String[] args) {
-
 		int x = 10;
 		int y = 5;
-		int z = 0;
-		char symbol;
+		int result = 0;
+		char mathSign;
 
-		char [] symbolArray = { '+', '-', '*', '/', '^' , '%'};
+		char [] mathSigns = {'+', '-', '*', '/', '^', '%'};
 
-		for (int i = 0; i < 6 ; i++) {
-			symbol = symbolArray[i];
+		for (int i = 0; i < 6; i++) {
+			mathSign = mathSigns[i];
 
-			if (symbol == '+') {
-				z = x + y;
-				System.out.println("sum " + x + " + " + y + " = " + z);
+			if (mathSign == '+') {
+				result = x + y;
+				System.out.println("sum " + x + " + " + y + " = " + result);
 			} else {
 				System.out.println("the sign of the mathematical operation not the sum is selected");
 			}
 
-			if (symbol == '-') {
-				z = x - y;
-				System.out.println("subtraction " + x + " - " + y + " = " + z);
+			if (mathSign == '-') {
+				result = x - y;
+				System.out.println("subtraction " + x + " - " + y + " = " + result);
 			} else {
 				System.out.println("the sign of the mathematical operation not the subtraction is selected");
 			}
 
-			if (symbol == '*') {
-				z = x * y;
-				System.out.println("multiplication " + x + " * " + y + " = " + z);
+			if (mathSign == '*') {
+				result = x * y;
+				System.out.println("multiplication " + x + " * " + y + " = " + result);
 			} else {
 				System.out.println("the sign of the mathematical operation not the multiplication is selected");
 			}
 
-			if (symbol == '/') {
-				z = x / y;
-				System.out.println("div " + x + " / " + y + " = " + z);
+			if (mathSign == '/') {
+				result = x / y;
+				System.out.println("div " + x + " / " + y + " = " + result);
 			} else {
 				System.out.println("the sign of the mathematical operation not the div is selected");
 			}
 
-			if (symbol == '^') {
-				z = x * x;
-				System.out.println("exponentiation 2 number " + x + " = " + z);
+			if (mathSign == '^') {
+				result = x * x;
+				System.out.println("exponentiation 2 number " + x + " = " + result);
+				result = y * y;
+				System.out.println("exponentiation 2 number " + y + " = " + result);
 			} else {
 				System.out.println("the exponentiation operation is not selected");
 			}
 
-			if (symbol == '%') {
-				z = x % y;
-				System.out.println("remainder of division " + x + " % " + y + " = " + z);
+			if (mathSign == '%') {
+				result = x % y;
+				System.out.println("remainder of division " + x + " % " + y + " = " + result);
 			} else {
 				System.out.println("the operation for finding the remainder of a division is not selected");
 			}
