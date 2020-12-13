0847a4c2f6dc837d7176f5f9518073529a07f97b
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-13 21:30:51 +0300

    delete unicodeSign

--- lesson 1/Unicode.java
+++ lesson 1/Unicode.java
@@ -1,10 +1,8 @@
 public class Unicode {
 	public static void main(String[] args) {
-		char unicodeSign;
 
 		for (char i = '\u0021'; i < '\u007F'; i++) {
-			unicodeSign = i;
-			System.out.println(unicodeSign);
+			System.out.println((char) i);
 		}
 	}
 }
