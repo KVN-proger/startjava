9231f86184a9b4b850e841f93f7c1e4bcdd8842e
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-13 21:37:08 +0300

    added file Person.java

---
+++ lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+	String personName = "Victor";
+	String gender = "Man";
+	int height = 175;
+	int weight = 65;
+	int age = 50;
+
+	void walk() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	void speak() {
+
+	}
+
+	void learnJava() {
+
+	}
+}
