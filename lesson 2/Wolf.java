3997557bc280c8e6c3df93c2f5c12ab370be16e4
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-15 20:27:51 +0300

    added file Wolf.java

---
+++ lesson 2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+	String gender = "male";
+	String nickname = "Barbos";
+	int weight = 25;
+	int age = 10;
+	String color = "gray";
+
+	void walk() {
+		System.out.println("the wolf walks");
+	}
+
+	void sit() {
+		System.out.println("the wolf is sitting");
+	}
+
+	void run() {
+		System.out.println("the wolf runs");
+	}
+
+	void howl() {
+		System.out.println("the wolf howl");
+	}
+
+	void hunt() {
+		System.out.println("the wolf hunt");
+	}
+}
