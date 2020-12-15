ac058f5234c060ce815d37723e4a7b557748a44f
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-15 20:29:58 +0300

    added file WolfTest.java

---
+++ lesson 2/WolfTest.java
@@ -0,0 +1,15 @@
+public class WolfTest {
+	public static void main(String[] args) {
+		Wolf wolfOne = new Wolf();
+		System.out.println("gender of the wolf - " + wolfOne.gender);
+		System.out.println("nickname of the wolf - " + wolfOne.nickname);
+		System.out.println("weight of the wolf - " + wolfOne.weight);
+		System.out.println("age of the wolf - " + wolfOne.age);
+		System.out.println("color of the wolf - " + wolfOne.color);
+		wolfOne.walk();
+		wolfOne.sit();
+		wolfOne.run();
+		wolfOne.howl();
+		wolfOne.hunt();
+	}
+}
