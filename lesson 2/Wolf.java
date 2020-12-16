f46c6698d3cf9344862ad3699e75b15034088449
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-16 21:56:38 +0300

    modified class Wolf.java

--- lesson 2/Wolf.java
+++ lesson 2/Wolf.java
@@ -1,28 +1,64 @@
 public class Wolf {
 
-	String gender;
-	String nickname;
-	int weight;
-	int age;
-	String color;
+	private String gender = "male";
+	private String nickname = "Barbos";
+	private int weight;
+	private int age;
+	private String color;
 
-	void walk() {
+	public String getGender() {
+		return gender;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("incorrect age");
+		} else {
+			this.age = age;
+		}
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void walk() {
 		System.out.println("the wolf walks");
 	}
 
-	void sit() {
+	public void sit() {
 		System.out.println("the wolf is sitting");
 	}
 
-	void run() {
+	public void run() {
 		System.out.println("the wolf runs");
 	}
 
-	void howl() {
+	public void howl() {
 		System.out.println("the wolf howl");
 	}
 
-	void hunt() {
+	public void hunt() {
 		System.out.println("the wolf hunt");
 	}
 }
