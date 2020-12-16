b2d2197c2ef933241ccdee4e3a8faa4d2013d2c9
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-16 20:40:16 +0300

    correction of comments

--- lesson 2/WolfTest.java
+++ lesson 2/WolfTest.java
@@ -1,6 +1,11 @@
 public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolfOne = new Wolf();
+		wolfOne.gender = "male";
+		wolfOne.nickname = "Barbos";
+		wolfOne.weight = 25;
+		wolfOne.age = 10;
+		wolfOne.color = "gray";
 		System.out.println("gender of the wolf - " + wolfOne.gender);
 		System.out.println("nickname of the wolf - " + wolfOne.nickname);
 		System.out.println("weight of the wolf - " + wolfOne.weight);
