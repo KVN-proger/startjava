c74e1c050e12bb5f22aed0ff9b805f4e3063cb4c
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-29 00:03:22 +0300

    correction file GuessNumberTest.java

--- lesson 2/GuessNumberTest.java
+++ lesson 2/GuessNumberTest.java
@@ -3,43 +3,19 @@ import java.util.Scanner;
 public class GuessNumberTest {
 	public static void main(String[] args) {
 		String confirmation;
-		boolean isWin = false;
-		int guessNumber;
 		Scanner sc = new Scanner(System.in);
 		GuessNumber gn = new GuessNumber();
 		System.out.println("enter the name of the first player");
 		Player playerOne = new Player(sc.next());
-		System.out.println("the name of the first player - " + playerOne.representName());
+		System.out.println("the name of the first player - " + playerOne.getName());
+		gn.setNamePlayerOne(playerOne.getName());
 		System.out.println("enter the name of the second player");
 		Player playerTwo = new Player(sc.next());
-		System.out.println("the name of the second player - " + playerTwo.representName());
+		System.out.println("the name of the second player - " + playerTwo.getName());
+		gn.setNamePlayerTwo(playerTwo.getName());
 
 		do {
-			guessNumber = gn.generateRandomNumber();
-			System.out.println("A random number is guessed - " + guessNumber);
-			isWin = false;
-			do {
-				System.out.println("player " + playerOne.representName() + " enter a number");
-				playerOne.setNumber(sc.nextInt());
-				System.out.println("player " + playerOne.representName() + " entered a number - " + playerOne.getNumber());
-				if (playerOne.getNumber() == guessNumber) {
-					System.out.println("player " + playerOne.representName() + " wins");
-					isWin = true;
-					break;
-				} else {
-					System.out.println("player " + playerOne.representName() + " entered wrong number");
-				}
-				System.out.println("player " + playerTwo.representName() + " enter a number");
-				playerTwo.setNumber(sc.nextInt());
-				System.out.println("player " + playerTwo.representName() + " entered a number - " + playerTwo.getNumber());
-				if (playerTwo.getNumber() == guessNumber) {
-					System.out.println("player " + playerTwo.representName() + " wins");
-					isWin = true;
-					break;
-				} else {
-					System.out.println("player " + playerTwo.representName() + " entered wrong number");
-				}
-			} while (!isWin);
+			gn.playGame();
 			do {
 				System.out.println("do you want to continue yes/no");
 				confirmation = sc.next();
