d893b26b0330e608c5274845c2620fdced6a1e77
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-29 22:07:40 +0300

    correction file GuessNumberTest.java

--- lesson 2/GuessNumberTest.java
+++ lesson 2/GuessNumberTest.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
 	public static void main(String[] args) {
-		String confirmation;
 		Scanner sc = new Scanner(System.in);
-		GuessNumber gn = new GuessNumber();
 		System.out.println("enter the name of the first player");
 		Player playerOne = new Player(sc.next());
 		System.out.println("the name of the first player - " + playerOne.getName());
-		gn.setNamePlayerOne(playerOne.getName());
 		System.out.println("enter the name of the second player");
 		Player playerTwo = new Player(sc.next());
 		System.out.println("the name of the second player - " + playerTwo.getName());
-		gn.setNamePlayerTwo(playerTwo.getName());
-
+		GuessNumber game = new GuessNumber(playerOne.getName(), playerTwo.getName());
+		String confirmation;
 		do {
-			gn.playGame();
+			game.start();
 			do {
 				System.out.println("do you want to continue yes/no");
 				confirmation = sc.next();
