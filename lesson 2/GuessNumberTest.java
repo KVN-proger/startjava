bd38c173027d70c9e17cf2e00aa4c46e2af9b544
Vitaly <supercyfqgth2010@yandex.ru>
2020-12-30 17:38:07 +0300

    correction file GuessNumberTest.java

--- lesson 2/GuessNumberTest.java
+++ lesson 2/GuessNumberTest.java
@@ -5,11 +5,9 @@ public class GuessNumberTest {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the name of the first player");
 		Player playerOne = new Player(sc.next());
-		System.out.println("the name of the first player - " + playerOne.getName());
 		System.out.println("enter the name of the second player");
 		Player playerTwo = new Player(sc.next());
-		System.out.println("the name of the second player - " + playerTwo.getName());
-		GuessNumber game = new GuessNumber(playerOne.getName(), playerTwo.getName());
+		GuessNumber game = new GuessNumber(playerOne, playerTwo);
 		String confirmation;
 		do {
 			game.start();
