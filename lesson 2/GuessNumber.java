7761bc04c7278b5a05ba9b45dd8bd69b97a387f0
Vitaly <supercyfqgth2010@yandex.ru>
2021-01-01 16:59:28 +0300

    correction file GuessNumber.java

--- lesson 2/GuessNumber.java
+++ lesson 2/GuessNumber.java
@@ -2,17 +2,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
-	int guessNumber;
+	private int guessNumber;
 	Scanner sc = new Scanner(System.in);
 	private Player plOne;
 	private Player plTwo;
 	Random randomNumber = new Random();
 
-	public GuessNumber (Player plOne, Player plTwo) {
+	public GuessNumber(Player plOne, Player plTwo) {
 		this.plOne = plOne;
 		this.plTwo = plTwo;
-
 	}
+
 	public void start() {
 		guessNumber = randomNumber.nextInt(100);
 		System.out.println("A random number is guessed - " + guessNumber);
@@ -26,6 +26,11 @@ public class GuessNumber {
 				break;
 			}
 			System.out.println("player " + plOne.getName() + " entered wrong number");
+			if (plOne.getNumber() < guessNumber) {
+				System.out.println("the number that player " + plOne.getName() + " entered is less than what the computer guessed");
+			} else {
+				System.out.println("the number that player " + plOne.getName() + " entered is greater than what the computer guessed");
+			}
 			System.out.println("player " + plTwo.getName() + " enter a number");
 			plTwo.setNumber(sc.nextInt());
 			System.out.println("player " + plTwo.getName() + " entered a number - " + plTwo.getNumber());
@@ -34,6 +39,11 @@ public class GuessNumber {
 				break;
 			}
 			System.out.println("player " + plTwo.getName() + " entered wrong number");
+			if (plTwo.getNumber() < guessNumber) {
+				System.out.println("the number that player " + plTwo.getName() + " entered is less than what the computer guessed");
+			} else {
+				System.out.println("the number that player " + plTwo.getName() + " entered is greater than what the computer guessed");
+			}
 		} while (true);
 	}
 }
