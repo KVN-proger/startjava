dbc305937f6090703d306bf2a6c0ebefd1ab136e
Vitaly <supercyfqgth2010@yandex.ru>
2021-01-09 16:05:02 +0300

    correction files Calculator.java, CalculatorTest.java

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -6,49 +6,64 @@ public class Calculator {
 	private int x;
 	private int y;
 	private char mathSign;
+	private String mathStatement;
 	public int result;
+	
+	public Calculator(String mathStatement) {
+		this.mathStatement = mathStatement;
+	}
+	public String getMathStatement() {
+		return mathStatement;
+	}
 
 	public void setX(int x) {
 		this.x = x;
 	}
 
-	public void setY(int y) {
-		this.y = y;
+	public int getX() {
+		return x;
 	}
 
 	public void setMathSign(char mathSign) {
 		this.mathSign = mathSign;
 	}
 
-	public void calculate() {
+	public char getMathSign() {
+		return mathSign;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int calculate() {
 		switch (mathSign) {
-			case '+' :
+			case '+':
 				result = addExact(x, y);
-				System.out.println("sum " + x + " + " + y + " = " + result);
 				break;
-			case '-' :
+			case '-':
 				result = subtractExact(x, y);
-				System.out.println("subtraction " + x + " - " + y + " = " + result);
 				break;
-			case '*' :
+			case '*':
 				result = multiplyExact(x, y);
-				System.out.println("multiplication " + x + " * " + y + " = " + result);
 				break;
-			case '/' :
-				result = y / x;
-				System.out.println("div " + y + " / " + x + " = " + result);
+			case '/':
+				result = x / y;
 				break;
-			case '^' :
+			case '^':
 				result = 1;
 				for (int a = 0; a < y; a++) {
 					result *= x;
 				}
-				System.out.println("exponentiation " + y + " number " + x + " = " + result);
 				break;
-			case '%' :
-				result = y % x;
-				System.out.println("remainder of division " + y + " % " + x + " = " + result);
+			case '%':
+				result = x % y;
 				break;
 		}
+		return result;
 	}
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 public class CalculatorTest {
 	public static void main(String[] args) {
 		String confirmation;
-		Calculator calc = new Calculator();
 		Scanner sc = new Scanner(System.in);
 		String str;
 
 		do {
 			System.out.println("enter a mathematical expression : 2 ^ 10");
-			str = (sc.nextLine());
-			String[] mathStatement = str.split(" ");
+			str = sc.nextLine();
+			Calculator calc = new Calculator(str);
+			String[] mathStatement = calc.getMathStatement().split(" ");
 
 			while (mathStatement[0].equals("")) {
-				str = (sc.nextLine());
-				mathStatement = str.split(" ");
+				str = sc.nextLine();
+				calc = new Calculator(str);
+				mathStatement = calc.getMathStatement().split(" ");
 			}
 			calc.setX(Integer.parseInt(mathStatement[0]));
 			calc.setMathSign(mathStatement[1].charAt(0));
 			calc.setY(Integer.parseInt(mathStatement[2]));
-			calc.calculate();
 
+			if (calc.getMathSign() == '+' || calc.getMathSign() == '-' || calc.getMathSign() == '*' || calc.getMathSign() == '/' || calc.getMathSign() == '%') {
+				System.out.println(calc.getX() + " " + calc.getMathSign() + " " + calc.getY() + " = " + calc.calculate());
+			} else if (calc.getMathSign() == '^') {
+				System.out.println("exponentiation " + calc.getY() + " number " + calc.getX() + " = " + calc.calculate());
+			}
 			do {
 				System.out.println("do you want to continue calculating? yes/no");
 				confirmation = sc.next();
