e5d515a154120d129953478a9a38ba355b80c15c
Vitaly <supercyfqgth2010@yandex.ru>
2021-01-10 22:07:13 +0300

    correction files CalculatorTest.java

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -9,20 +9,13 @@ public class CalculatorTest {
 		String str;
 
 		do {
+			str = null;
 			System.out.println("enter a mathematical expression : 2 ^ 10");
-			str = sc.nextLine();
-			Calculator calc = new Calculator(str);
-			String[] mathStatement = calc.getMathStatement().split(" ");
 
-			while (mathStatement[0].equals("")) {
+			while (str == null || str.equals("")) {
 				str = sc.nextLine();
-				calc = new Calculator(str);
-				mathStatement = calc.getMathStatement().split(" ");
 			}
-			calc.setX(Integer.parseInt(mathStatement[0]));
-			calc.setMathSign(mathStatement[1].charAt(0));
-			calc.setY(Integer.parseInt(mathStatement[2]));
-
+			Calculator calc = new Calculator(str);
 			if (calc.getMathSign() == '+' || calc.getMathSign() == '-' || calc.getMathSign() == '*' || calc.getMathSign() == '/' || calc.getMathSign() == '%') {
 				System.out.println(calc.getX() + " " + calc.getMathSign() + " " + calc.getY() + " = " + calc.calculate());
 			} else if (calc.getMathSign() == '^') {
